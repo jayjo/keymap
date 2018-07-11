a0091525bed417e74d2331514f16c4727addc07d
Jeffrey Jorgensen <jeff@jayjodesign.com>
2018-07-11 11:24:03 -0600

    added in the keypad

--- source/javascript/custom/keypad.js
+++ source/javascript/custom/keypad.js
@@ -1,4 +1,17 @@
 jQuery(document).ready(function($) {
-  // Draggable Menu
-  $('#keypad').draggable();
+  
+  $(".expand").on('click', function(e){
+    $(this).addClass('inactive');
+    $('.minimize').removeClass('inactive');
+    $('.keypad').draggable();
+    $('.keypad').addClass('undocked');
+  });
+
+  $(".minimize").on('click', function(e){
+    $(this).addClass('inactive');
+    $('.expand').removeClass('inactive');
+    $('.keypad').draggable('destroy');
+    $('.keypad').removeClass('undocked').attr('style', '')
+    $('.keypad').removeClass('undocked');
+  });
 });
